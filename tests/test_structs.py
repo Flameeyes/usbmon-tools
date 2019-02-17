a0a23d9a904d3321fc44f5d76d65de0ef581e154
Diego Elio Pettenò <flameeyes@flameeyes.com>
2019-02-17 17:25:48 +0000

    Add Tests for control packets as well.
    The control packets are more complicated, as the setup packet need to be printed out as well.

--- tests/test_structs.py
+++ tests/test_structs.py
@@ -15,19 +15,40 @@ _INTERRUPT_S_BASE64 = (
     'gLi22gAAAABTAYECAQAtPMkvaFwAAAAA9/wDAI3///8IAAAAAAAAAAAAAAAAAAAACAAAAAAAAA'
     'AEAgAAAAAAAA==')
 
+_CONTROL_S_BASE64 = (
+    'AKrN2gAAAABTAoABAQAAPMUvaFwAAAAAuNIBAI3///8oAAAAAAAAAIAGAAEAACgAAAAAAAAAAA'
+    'AAAgAAAAAAAA==')
+_CONTROL_C_BASE64 = (
+    'AKrN2gAAAABDAoABAQAtAMUvaFwAAAAAX9MBAAAAAAASAAAAEgAAAAAAAAAAAAAAAAAAAAAAAA'
+    'AAAgAAAAAAABIBAAIJAAFAax0CABQEAwIBAQ==')
+
 
 class TestPacket(absltest.TestCase):
 
-    def test_string_c_packet(self):
-        c_packet = usbmon.structs.Packet.from_bytes(
+    def test_string_interrupt_c(self):
+        packet = usbmon.structs.Packet.from_bytes(
             '<', binascii.a2b_base64(_INTERRUPT_C_BASE64))
         self.assertEqual(
             'dab6b880 1550331849261217 C Ii:1:002:1 0:8 8 = 01000000 00000000',
-            str(c_packet))
+            str(packet))
 
-    def test_string_s_packet(self):
-        s_packet = usbmon.structs.Packet.from_bytes(
+    def test_string_interrupt_s(self):
+        packet = usbmon.structs.Packet.from_bytes(
             '<', binascii.a2b_base64(_INTERRUPT_S_BASE64))
         self.assertEqual(
             'dab6b880 1550331849261367 S Ii:1:002:1 -115:8 8 <',
-            str(s_packet))
+            str(packet))
+
+    def test_string_control_s(self):
+        packet = usbmon.structs.Packet.from_bytes(
+            '<', binascii.a2b_base64(_CONTROL_S_BASE64))
+        self.assertEqual(
+            'dacdaa00 1550331845119480 S Ci:1:001:0 s 80 06 0100 0000 0028 40 <',
+            str(packet))
+
+    def test_string_control_c(self):
+        packet = usbmon.structs.Packet.from_bytes(
+            '<', binascii.a2b_base64(_CONTROL_C_BASE64))
+        self.assertEqual(
+            'dacdaa00 1550331845119647 C Ci:1:001:0 0 18 = 12010002 09000140 6b1d0200 14040302 0101',
+            str(packet))
