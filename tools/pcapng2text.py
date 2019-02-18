6dc5cfdb280ad7063fd32881398016dd350b7eea
Diego Elio Pettenò <flameeyes@flameeyes.com>
2019-02-18 08:11:48 +0000

    Document what pcapng2text does.

--- tools/pcapng2text.py
+++ tools/pcapng2text.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 #
 # Copyright 2019 The usbmon-tools Authors
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     https://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,23 @@
 # limitations under the License.
 #
 # SPDX-License-Identifier: Apache-2.0
+"""Convert a pcapng usbmon capture to usbmon text format.
+
+This is a simple tool that allows generating usbmon text format output based on
+the binary (mmap) API.
+
+It tries to stay as close to the current Linux text format as possible, although
+with a few notable changes:
+
+ - URBs can be optionally retagged, so that each Submission/Callback pair has a
+   unique tag. This is unlike the Linux interface, where the tag is the
+   in-memory pointer to the URB structure, and is thus repeated in the same
+   capture, as structures are reused. When this is enabled, the tag is a UUID,
+   and thus much longer than the kernel's own tag.
+
+ - The timestamp for the captures is provided in full, rather than being
+   constrained to 32-bit range.
+"""
 
 import usbmon.pcapng
 
