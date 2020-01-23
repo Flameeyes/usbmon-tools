768860fae6e28fc002b4609b7cd74d93ba3eeec3
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-01-23 22:16:04 +0000

    Expand the chatter interface to allow prefixes, and empty prints.
    This includes tests to maintain the interface stable in the future.
    
    The chatter format is one of my strong points for usbmon-tools and I want
    to make it possible to use it in custom extractors as well.

---
+++ tests/test_chatter.py
@@ -0,0 +1,76 @@
+# python
+#
+# Copyright 2020 The usbmon-tools Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# SPDX-License-Identifier: Apache-2.0
+"""Tests for usbmon.chatter."""
+
+from absl.testing import absltest
+
+import usbmon.chatter
+import usbmon.constants
+
+
+class DumpBytesTest(absltest.TestCase):
+
+    def test_simple(self):
+        self.assertEqual(
+            usbmon.chatter.dump_bytes(
+                usbmon.constants.Direction.IN,
+                b'\x01\x02\x03'),
+            'H<<D 00000000: 01 02 03                                          ...')
+
+        self.assertEqual(
+            usbmon.chatter.dump_bytes(
+                usbmon.constants.Direction.OUT,
+                b'\x01\x02\x03'),
+            'H>>D 00000000: 01 02 03                                          ...')
+
+    def test_empty(self):
+        self.assertEqual(
+            usbmon.chatter.dump_bytes(
+                usbmon.constants.Direction.OUT,
+                b''),
+            '')
+
+    def test_print_empty(self):
+        self.assertEqual(
+            usbmon.chatter.dump_bytes(
+                usbmon.constants.Direction.OUT,
+                b'',
+                print_empty=True,
+            ),
+            'H>>D 00000000:'
+        )
+
+    def test_custom_prefix(self):
+        self.assertEqual(
+            usbmon.chatter.dump_bytes(
+                usbmon.constants.Direction.IN,
+                b'\x01\x02\x03',
+                prefix='pfx',
+            ),
+            'pfx H<<D 00000000: 01 02 03                                          ...')
+
+    def test_custom_prefix_empty(self):
+        self.assertEqual(
+            usbmon.chatter.dump_bytes(
+                usbmon.constants.Direction.OUT,
+                b'',
+                prefix='pfx',
+                print_empty=True,
+            ),
+            'pfx H>>D 00000000:'
+        )
--- usbmon/chatter.py
+++ usbmon/chatter.py
@@ -1,6 +1,6 @@
 # python
 #
-# Copyright 2019 The usbmon-tools Authors
+# Copyright 2019-2020 The usbmon-tools Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,8 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from typing import Optional
+
 import hexdump
 
 from usbmon import constants
@@ -27,14 +29,44 @@ _DIRECTION_TO_PREFIX = {
 }
 
 
-def dump_bytes(direction: constants.Direction, payload: bytes) -> str:
+def dump_bytes(
+        direction: constants.Direction,
+        payload: bytes,
+        prefix: Optional[str] = None,
+        print_empty: bool = False,
+) -> str:
+    """Return a "chatter" string for the provided payload.
+
+    This function provides a hexdump-based output for a given binary payload,
+    including a direction marker prefix, and an optional customized prefix.
+
+    Args:
+      direction: The direction the payload traveled.
+      payload: The actual payload to dump.
+      prefix: If provided, this string will be added in front of each output
+        line.
+      print_empty: Whether to print an empty dump for zero-length payloads.
+
+    Returns:
+      A multi-line string suitable for printing to standard output.
+    """
+    line_prefix = _DIRECTION_TO_PREFIX[direction]
+
+    if prefix:
+        line_prefix = f'{prefix} {line_prefix}'
+
     if not payload:
-        return ''
+        if not print_empty:
+            return ''
+        else:
+            # This is tricky, hexdump does not actually do what we want it to,
+            # so we fake it.
+            return f'{line_prefix}00000000:'
 
     hexd = hexdump.dumpgen(payload)
-    return '\n'.join(''.join((_DIRECTION_TO_PREFIX[direction], hexrow))
-                     for hexrow in hexd)
+    return '\n'.join(
+        ''.join((line_prefix, hexrow)) for hexrow in hexd)
 
 
-def dump_packet(packet: structs.Packet) -> str:
-    return dump_bytes(packet.direction, packet.payload)
+def dump_packet(packet: structs.Packet, **kwargs) -> str:
+    return dump_bytes(packet.direction, packet.payload, **kwargs)
