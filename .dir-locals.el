80bbbb5c27fe42a62012047f56c68f9736a3ef42
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-03-26 18:43:13 +0000

    Update the local Emacs config to match the black/isort/flake8 configs.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
-((nil . ((fill-column . 80)))
+((nil . ((fill-column . 88)
+	 (whitespace-line-column . 88)))
  (markdown-mode . ((mode . flyspell)))
  (python-mode . ((mode . flyspell-prog)
 		 (mode . whitespace))))
