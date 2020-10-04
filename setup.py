51ff1ffb871ca47bab15ac5d043b7229c2bbf902
Diego Elio Pettenò <flameeyes@flameeyes.com>
2020-10-04 12:38:43 +0100

    Move the definition of entry points to setup.cfg.

--- setup.py
+++ setup.py
@@ -8,14 +8,4 @@ from setuptools import setup
 # Ensure it's present.
 import setuptools_scm  # noqa: F401
 
-setup(
-    entry_points={
-        "console_scripts": [
-            "usbmon-capture_stats=usbmon.tools.capture_stats:main",
-            "usbmon-chatter_cp2110=usbmon.tools.chatter_cp2110:main",
-            "usbmon-chatter_hid=usbmon.tools.chatter_hid:main",
-            "usbmon_pcapng2base64=usbmon.tools.pcapng2base64:main",
-            "usbmon-pcapng2text=usbmon.tools.pcapng2text:main",
-        ]
-    },
-)
+setup()
